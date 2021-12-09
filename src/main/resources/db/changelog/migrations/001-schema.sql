ca822eb6bffa261696fc8bf62b614f1623073f5e
lamfalusy <lamfalusy@gmail.com>
2021-12-09 20:37:18 +0100

    Application skeleton

---
+++ src/main/resources/db/changelog/migrations/001-schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Persons (
+    id int,
+    name varchar(255),
+    message varchar(255),
+    PRIMARY KEY (id)
+);
---
+++ src/test/java/hu/lamsoft/macau/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.lamsoft.macau.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
